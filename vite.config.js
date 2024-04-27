ad7ae784efdf518195bc1457b1ccaf7b82fe5926
Shubhendu-Maharana <maharanasubhendu3@gmail.com>
2024-04-27 11:23:52 +0530

    again:

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/computer-quiz-app/"
+  base: "/computer-quiz-app/",
   plugins: [react()],
 })
